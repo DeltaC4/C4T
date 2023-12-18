080025bc89f4f5205567c6a61c0fb76043505192
clad2720 <d.clarke@uleth.ca>
2023-12-17 17:19:52 -0700

    code cleanup

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 function generator() {
- //   debugger;
     var x = Math.floor((Math.random()* 20)+1);
-    console.log(x);
     var imgUrl = "<img src='img/pic" + x + ".jpg'>";
     document.getElementById('divImage').innerHTML= imgUrl;
 }
